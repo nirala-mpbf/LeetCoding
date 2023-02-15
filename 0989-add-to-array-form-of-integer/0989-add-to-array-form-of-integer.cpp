96a6865e146310f60c0e50509d0bdb966820b641
Shashi Nirala <121391477+nirala-mpbf@users.noreply.github.com>
2023-02-16 03:09:23 +0530

    Time: 26 ms (87.90%), Space: 27.4 MB (65.67%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -7,7 +7,10 @@ public:
             k= num[i]/10;
             num[i]=num[i]%10;
         }
-        
+
+/*
+//Method 1:
+
         if(k==0){
             return num;
         }
@@ -23,6 +26,15 @@ public:
             ans.push_back(x);
         }
         return ans;
-            
+*/
+    
+// method2:
+        while(k!=0){
+            //insering in begin of num
+            num.insert(num.begin() , k%10);
+            k=k/10;
+        }
+        
+        return num;
     }
 };
